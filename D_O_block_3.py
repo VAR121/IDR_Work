314e8f07ea8ba80426265d1ba2fdea371cbca46a
var121 <aravind121@gmail.com>
2015-12-08 17:34:34 +0530

    Working Copy with terminal issues

--- D_O_block_3.py
+++ D_O_block_3.py
@@ -11,109 +11,104 @@ inp_1 = open(in_1, 'r')
 input_lines_1 = inp_1.readlines()
 inp_1.close()
 
-length_1 = len(input_lines_1) 
-d_sres = 0
+length_1 = len(input_lines_1)
+
+sres = 0
 d_count = 0
 o_count = 0
 n_count = 0
-d_eres = 0
+eres = 0
 
 line = 0
 
 while line < length_1:
 
-	in_file = input_lines_1[line]
-	lines = in_file.strip('\n')
+    in_file = input_lines_1[line]
+    lines = in_file.strip('\n')
 
-	if lines == "D":
-		if d_count == 0:
-			d_sres = (line + 1)  # Starting boundary
-		
-		d_count += 1
-	        
-		if n_count < 3:
-			n_count = 0
-		if o_count < 3:
-			o_count = 0 
-    
-	if (d_count != 0) and lines == "O":
-		o_count += 1
- 
-        
-	if (d_count != 0) and lines == "N":
-		n_count += 1
+    if lines == "D":
+
+        if d_count == 0:
+            sres = line + 1  # Starting boundary
+
+        d_count += 1
+
+        if n_count < 4:
+            n_count = 0
+        if o_count < 4:
+            o_count = 0
+
+    if (d_count != 0) and lines == "O":
+        o_count += 1
+
+    if (d_count != 0) and lines == "N":
+        n_count += 1
+
+    if (d_count != 0) and ((o_count == 4) or (n_count == 4) or ((o_count + n_count) == 4)):
+
+        if (d_count <= 3):
+            o_count = n_count = d_count = sres = 0
+            line += 1
+            continue
+
+        eres = (line - 3)
+        line = eres
+
+        print("D", sres, eres, sep='\t')
+
+    if eres > 0:
+        sres = 0
+        d_count = 0
+        o_count = 0
+        n_count = 0
+        eres = 0
+
+    line += 1
 
 
-	if  (d_count != 0) and ((o_count > 3) or (n_count > 3) or ((o_count + n_count) > 3 )):
-#		d_eres = (line - 3)
-#		line = d_eres
+sres = eres = line = 0
 
-		if (d_count < 4) :
-			o_count = 0
-			n_count = 0
-			d_count = 0
-			d_sres = 0
-#			d_eres =0
-#			line += 1
-#			continue
-
-		d_eres = (line - 3)
-		line = d_eres
-		print((line+1), o_count,d_count,n_count,lines,d_sres,d_eres,sep='\t')			
-
-
-	if d_eres > 0:
-		d_sres = 0
-		d_count = 0
-		o_count = 0
-		n_count = 0
-		d_eres = 0    
-
-	line += 1       
-
-o_sres = 0
-o_eres = 0
-line = 0
 while line < length_1:
 
-	in_file = input_lines_1[line]
-	lines = in_file.strip('\n')
+    in_file = input_lines_1[line]
+    lines = in_file.strip('\n')
 
-	if lines == "O":
-		if o_count == 0:
-			o_sres = (line + 1)  # Starting boundary
-		o_count += 1
+    if lines == "O":
 
-		if n_count < 3:
-			n_count = 0
-		if d_count < 3:
-			d_count = 0
+        if o_count == 0:
+            sres = line + 1  # Starting boundary
 
-	if (o_count != 0) and lines == "D":
-		d_count += 1
+        o_count += 1
 
+        if n_count < 4:
+            n_count = 0
+        if d_count < 4:
+            d_count = 0
 
-	if (o_count != 0) and lines == "N":
-		n_count += 1
+    if (o_count != 0) and lines == "D":
+        d_count += 1
 
+    if (o_count != 0) and lines == "N":
+        n_count += 1
 
-	if  (o_count != 0) and ((d_count > 3) or (n_count > 3) or ((d_count + n_count) > 3 )):
-		o_eres = (line - 3)
-		if (o_count < 4) :
-			o_count = 0
-			o_sres = 0
-           
-		line = o_eres
-		print((line+1), o_count,d_count,n_count,lines,o_sres,o_eres,sep='\t')
-	
-	if o_eres > 0:
-		o_sres = 0
-		d_count = 0
-		o_count = 0
-		n_count = 0
-		o_eres = 0
+    if (o_count != 0) and ((d_count == 4) or (n_count == 4) or ((d_count + n_count) == 4)):
 
-	line += 1
+        if (o_count <= 3):
+            o_count = n_count = d_count = sres = 0
+            line += 1
+            continue
 
-exit()
-     
+        eres = (line - 3)
+        line = eres
+
+        print("O", sres, eres, sep='\t')
+        # print("Diag", line, o_count)
+
+    if eres > 0:
+        sres = 0
+        d_count = 0
+        o_count = 0
+        n_count = 0
+        eres = 0
+
+    line += 1
